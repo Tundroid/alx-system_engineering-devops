5ab6b6e6e0281888931c56da8b27a12b91cb8953
Tundroid <felixkimbu@outlook.com>
2024-05-08 03:53:56 +0100

    Tasks 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,35 +1,16 @@
-
 #!/usr/bin/python3
-"""Module to retrieve the number of subscribers for a subreddit"""
+"""Function to query subscribers on a given Reddit subreddit."""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Write a function that queries the Reddit API and returns
-    the number of subscribers (not active users, total subscribers)
-    for a given subreddit. If an invalid subreddit is given, the
-    function should return 0.
-
-    Hint: No authentication is necessary for most features of the Reddit
-    API. If you’re getting errors related to Too Many Requests, ensure you’re
-    setting a custom User-Agent.
-
-    Requirements:
-
-    Prototype: def number_of_subscribers(subreddit)
-    If not a valid subreddit, return 0.
-    NOTE: Invalid subreddits may return a redirect to search results. Ensure
-    that you are not following redirects.
-    """
-    import requests
-
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    header = {
-        'User-Agent': 'alx-client/1.0'
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-
-    data = requests.get(url, headers=header, allow_redirects=False)
-    status_code = data.status_code
-    if (status_code == 200):
-        return data.json()["data"]["subscribers"]
-    return 0
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
